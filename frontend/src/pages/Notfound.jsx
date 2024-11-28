9e11c3aba2c66cb364b24aa88a1b679581ca4bf5
Mintesinot Abebe <mintesinot05@gmail.com>
2024-11-28 20:17:12 +0300

    first commit

---
+++ frontend/src/pages/Notfound.jsx
@@ -0,0 +1,10 @@
+function Notfound() {
+    return 
+    <div>
+       <h1> 404 not found </h1>
+       <p>The page you're looking for doesn't exist!</p>
+    </div>
+    
+}
+
+export default Notfound
